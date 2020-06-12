22ba92907157dec5a87ba699676c9aaecb01826a
Gabriel <usuariomac@MacBook-Pro.local>
2020-06-12 04:15:34 -0500

    primera versión

---
+++ config/larafiel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Retorna la configuración de Larafiel necesaria para instanciar la API de Mifiel.
+ */
+return [
+    // Visualizar https://sandbox.mifiel.com/access_tokens para más información de los tokens.
+
+    /**
+     * APP_ID
+     */
+    'app_id' => env('MIFIEL_APP_ID', false),
+
+    /**
+     * APP_SECRET
+     */
+    'app_secret' => env('MIFIEL_APP_SECRET', false),
+
+    /**
+     * URL de la API que quieres utilizar en tu aplicación
+     */
+    'mifiel_url' => env('MIFIEL_URL', 'https://sandbox.mifiel.com/api/v1/')
+];
